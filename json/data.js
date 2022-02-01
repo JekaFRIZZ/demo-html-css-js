6207f2f85a72d2e4f594b018f3670412843a8521
Женя <lapko.eugene@gmail.com>
2022-02-01 20:16:58 +0300

    full project

---
+++ json/data.js
@@ -0,0 +1,58 @@
+const array = [{
+  "image": "../image/shopping_cart_black_24dp.svg",
+  "title": "First",
+  "description": "This is comp",
+  "expired": "7",
+  "price": 123,
+  "tag" : "Automobiles"
+},
+  {
+    "image": "../image/shopping_cart_black_24dp.svg",
+    "title": "Second",
+    "description": "Description",
+    "expired": "7",
+    "price": 123,
+    "tag" : "Film Animation"
+  },
+  {
+    "image": "../image/shopping_cart_black_24dp.svg",
+    "title": "Third",
+    "description": "Description",
+    "expired": "7",
+    "price": 123,
+    "tag" : "Automobiles"
+  },
+  {
+    "image": "../image/shopping_cart_black_24dp.svg",
+    "title": "Fourth",
+    "description": "Description",
+    "expired": "7",
+    "price": 123,
+    "tag" : "Film Animation"
+  },
+  {
+    "image": "../image/shopping_cart_black_24dp.svg",
+    "title": "Fifth",
+    "description": "Description",
+    "expired": "7",
+    "price": 123,
+    "tag" : "Automobiles"
+  },
+  {
+    "image": "../image/shopping_cart_black_24dp.svg",
+    "title": "Sixth",
+    "description": "Description",
+    "expired": "7",
+    "price": 123,
+    "tag" : "Film Animation"
+  },{
+  "image": "../image/shopping_cart_black_24dp.svg",
+  "title": "Seventh",
+  "description": "Description",
+  "expired": "7",
+  "price": 123,
+  "tag" : "Automobiles"
+}
+];
+
+export default array;
